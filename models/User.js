c577a3ceaa0a8489f96292f2d7fcc19fccdfbc1e
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-07-20 21:19:26 -0400

    imported packages for User model

---
+++ models/User.js
@@ -0,0 +1,9 @@
+const {Model, DataTypes} = require('sequelize');
+const bycrypt = require('bycrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+User.init({
+
+})
