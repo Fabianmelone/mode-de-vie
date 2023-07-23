ac1154a908e33d17516b878c31623e2e8ec35d76
PythonFabi <129299589+PythonFabi@users.noreply.github.com>
2023-07-23 18:40:25 -0400

    start creating models for comment, gallery, post, user and relationships between them

---
+++ models/Post.js
@@ -0,0 +1,58 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        image_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        likes: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 0,
+        },
+        saves: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 0,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = Post;
---
+++ models/index.js
@@ -0,0 +1,4 @@
+const Comment = require('./Comment');
+const Gallery = require('./Gallery');
+const Post = require('./Post');
+const User = require('./User');
