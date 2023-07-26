8d1feaf0f5850133986fc0f024a3c932db907175
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-07-26 19:59:27 -0400

    added condition to redirect to the default route when logout response is ok

--- public/js/logout.js
+++ public/js/logout.js
@@ -4,7 +4,13 @@ const logoutHandler = async (event) => {
     const response = await fetch('/api/users/logout', {
         method: 'Post',
         headers: {'Content-Type': 'application/js'},
-    })
+    });
+
+    if(response.ok) {
+        window.location.replace('/');
+    } else {
+        alert('failed to logout');
+    }
 }
 
 
