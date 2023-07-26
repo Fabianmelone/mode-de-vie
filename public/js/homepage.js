46d18861b7846e38b600b52b6ed5cc6b279c35b6
hnm938 <mrbooomboom@gmail.com>
2023-07-26 00:28:54 -0400

    Finishing homepage, already added static layout elements.

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -4,9 +4,10 @@ function toggleSidebar() {
   sidebar.classList.toggle("closed");
 }
 
-async function ImgToSvg(img) {
+async function ImgToSvg(img, id) {
   const s = document.createElement('div');
   s.innerHTML = await (await fetch(img.src)).text();
   s.firstChild.classList = img.classList;
+  if (id) { s.firstChild.id = id; }
   img.replaceWith(s.firstChild)
 }
