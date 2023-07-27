dbb710d9a24fce3c6945d8c7bc63833fcb03700f
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-07-27 01:53:48 -0400

    added a comment to explain what `as` does

--- models/index.js
+++ models/index.js
@@ -34,7 +34,7 @@ Comment.belongsTo(Post, {
 //many-to-many relationshi0p between user and user.
 // this 'user' is followed by other users
 User.belongsToMany(User, { 
-    as: 'Followers', //ives an alias to  the relation when we are looking for all the followers of a user.
+    as: 'Followers', //uses an alias ('as') to  the relation when we are looking for all the followers of a user. "as" is used to differentiate between the two associations (following and follower)
     through: Follower_User, //use the Follower_User model as the join table to make this many-to-many relationship.
     foreignKey: 'user_id',  //the foreign key that the Follower_User model will  use to join with the User model.
     otherKey: 'follower_id' //the foreign key that the Follower_User model will use to join with the other instance of User model (in this case, followers). 
