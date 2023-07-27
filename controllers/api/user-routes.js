c613ebc2d000e30437fff131957a9bfe001657dd
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-07-27 10:18:02 -0400

    checks if the login request has an existing user, and if its password is correct

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -7,7 +7,12 @@ router.post('/login', async (req, res) => {
     try {
         const userData = await User.findOne({ where: { username: req.body.username } });    //finds a user where its username is the same as the username in the request body.
 
-        
+        const passwordData = userData.checkPass(req.body.password); //calls the checkPass function to verify if the password is correct.
+        //stores its callbak to the 'passwordData' const
+
+        if(!userData || !passwordData){
+            res.status(400).json({message: 'username or password incorrect! try again.'})
+        }
         
     } catch (error) {
         res.status(500).json(error);
