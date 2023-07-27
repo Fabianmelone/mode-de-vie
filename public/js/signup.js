3d2ec090570c012f63ea2447e151fb58d13e2201
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-07-26 20:34:24 -0400

    creeated a fetch request to the api/users/signup route in the controllers

--- public/js/signup.js
+++ public/js/signup.js
@@ -6,6 +6,19 @@ const signupHandler = async (event) => {
     const username = document.querySelector('#username').value.trim();
     const password = document.querySelector('#password').value.trim();
     const email = document.querySelector('#email').value.trim();
+
+    if (username && password && email) {    //if there is a username, password AND email, a post request will be posted to api/users/signup with its body as the username, password, and email
+
+        const response = await fetch('api/users/signup', {
+            method: 'POST',
+            body: JSON.stringify({username, password, email}),
+            headers: {'Content-Type': 'application/json'},
+        });
+
+        if(response.ok) {
+            
+        }
+    }
 }
 
 
