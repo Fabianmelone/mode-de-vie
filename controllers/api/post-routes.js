70fe3459dbca8d32a52c0d449bbd31e397c84b29
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-07-28 17:16:11 -0400

    created a route to show all posts of a user

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -4,7 +4,8 @@ const { Post, User, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 
-router.get('/', async (req, res) => {   //gets all posts
+// /api/post/
+router.get('/', withAuth, async (req, res) => {   //gets all posts
     try {
         const allPosts = await Post.findAll({
             include: [
@@ -33,17 +34,15 @@ router.get('/', async (req, res) => {   //gets all posts
         //     posts,
         //     logged_in: req.session.logged_in
         // });
-        console.log(req.session.logged_in);
+        console.log(req.session);
     } catch (error) {
         res.status(500).json(error);
     }
 });
 
 
-router.get('/popular', async (req, res) => {   //filter all posts from most likes
+router.get('/popular', withAuth, async (req, res) => {   //filter all posts from most likes
     try {
-
-
         const allPosts = await Post.findAll({
             order: [
                 ['likes', 'DESC']
@@ -68,18 +67,53 @@ router.get('/popular', async (req, res) => {   //filter all posts from most like
         });
 
         const posts = allPosts.map(post => post.get({ plain: true }));
-        console.log(posts);
+        // console.log(posts);
         res.json(posts);
 
         // res.render('homepage', {
         //     posts,
         //     logged_in: req.session.logged_in
         // });
-        console.log(req.session.logged_in);
+        console.log(req.session);
     } catch (error) {
         res.status(500).json(error);
     }
 });
+
+
+router.get('/userposts', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.userID, {
+            include: [
+                {
+                    model: Post,
+                    include: [
+                        {
+                            model: User,
+                            as: 'user',
+                            attributes: ['username']
+                        }
+                    ]
+                }
+            ]
+        });
+
+        const users = userData.get({plain: true});
+
+        res.render('homepage', {
+            ...users,
+            loggedIn: req.session.loggedIn
+        });
+        // console.log(users);
+        console.log(req.session);
+
+
+
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
 
 
 
