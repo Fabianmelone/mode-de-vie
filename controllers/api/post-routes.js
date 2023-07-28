6d333b31b3236eb18e7457c43fd857517f29b719
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-07-28 10:09:58 -0400

    added a filterLikes.js helper function to sort array by top likes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,20 +1,70 @@
 const router = require('express').Router();
-const  {Post, User}  = require('../../models/Post');
+const sequelize = require('../../config/connection');
+const  {Post, User}  = require('../../models');
 const withAuth = require('../../utils/auth');
 
 
-router.get('/', async (req, res) => {
+// router.get('/', async (req, res) => {   //gets all posts
+//     try {
+//         const allPosts = await Post.findAll({
+//             include: [
+//                 {
+//                     model: User,
+//                     attributes: ['username'],
+//                 }
+//             ],
+//         });
+//         const posts = allPosts.map((post) => post.get({ plain: true })); //maps over all elements of of allPosts and serialeze them
+//         //serialize to make the data easier to handle/reaD
+//         console.log(posts);
+//         res.json(posts);
+
+//         // res.render('homepage', {
+//         //     posts,
+//         //     logged_in: req.session.logged_in
+//         // });
+//         console.log(req.session.logged_in);
+//     } catch (error) {
+//         res.status(500).json(error);
+//     }
+// });
+
+
+router.get('/', async (req, res) => {   //filter all posts from most likes
     try {
+
+        const topLikes = await Post.max('likes');
         const allPosts = await Post.findAll({
-            include: [
-                {
-                    model: User,
-                    attributes: ['name'],
-                }
-            ]
-        })
+        });
+
+        const posts = allPosts.map((post) => {
+            
+
+            
+
+
+            post.get({ plain: true });
+
+
+        });
+        
+        
+        //maps over all elements of of allPosts and serialeze them
+        //serialize to make the data easier to handle/reaD
+        // console.log(posts);
+        res.json(posts);
+
+        // res.render('homepage', {
+        //     posts,
+        //     logged_in: req.session.logged_in
+        // });
+        console.log(req.session.logged_in);
     } catch (error) {
         res.status(500).json(error);
     }
-})
+});
+
+
+
+module.exports = router;
 
---
+++ utils/filterLikes.js
@@ -0,0 +1,6 @@
+const filterLikes = (arr, next) => {
+
+    
+
+}
+
