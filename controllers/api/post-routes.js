852cb0040b0317020f3e79ac91958cf5c61fc2d6
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-07-28 10:59:35 -0400

    filtered posts from greatest to least likes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -2,54 +2,48 @@ const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const { Post, User } = require('../../models');
 const withAuth = require('../../utils/auth');
-const filterLikes = require('../../utils/filterLikes')
 
 
-// router.get('/', async (req, res) => {   //gets all posts
-//     try {
-//         const allPosts = await Post.findAll({
-//             include: [
-//                 {
-//                     model: User,
-//                     attributes: ['username'],
-//                 }
-//             ],
-//         });
-//         const posts = allPosts.map((post) => post.get({ plain: true })); //maps over all elements of of allPosts and serialeze them
-//         //serialize to make the data easier to handle/reaD
-//         console.log(posts);
-//         res.json(posts);
+router.get('/', async (req, res) => {   //gets all posts
+    try {
+        const allPosts = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                }
+            ],
+        });
+        const posts = allPosts.map((post) => post.get({ plain: true })); //maps over all elements of of allPosts and serialeze them
+        //serialize to make the data easier to handle/reaD
+        console.log(posts);
+        res.json(posts);
 
-//         // res.render('homepage', {
-//         //     posts,
-//         //     logged_in: req.session.logged_in
-//         // });
-//         console.log(req.session.logged_in);
-//     } catch (error) {
-//         res.status(500).json(error);
-//     }
-// });
+        // res.render('homepage', {
+        //     posts,
+        //     logged_in: req.session.logged_in
+        // });
+        console.log(req.session.logged_in);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 
 
-router.get('/', async (req, res) => {   //filter all posts from most likes
+router.get('/most-popular', async (req, res) => {   //filter all posts from most likes
     try {
 
 
-        const allPosts = await Post.findAll({});
-
+        const allPosts = await Post.findAll({
+            order: [
+                ['likes', 'DESC']
+            ]
+        });
+    
         const posts = allPosts.map(post => post.get({ plain: true }));
-        
-        filterLikes(posts);
-        
-
-
-
-
-
-
         //maps over all elements of of allPosts and serialeze them
         //serialize to make the data easier to handle/reaD
-        // console.log(topLikes);
+        console.log(posts);
 
 
         res.json(posts);
