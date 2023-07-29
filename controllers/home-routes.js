b27ce5efee8eff006bb87853f23788a2fe82aa33
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-07-28 22:37:29 -0400

    added logic to have only the top 8 posts to a new array

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,51 +5,106 @@ const { Post, User, Comment } = require('../models');
 
 
 // Define the base route, runs login check before rendering
-router.get("/", withAuth,  async (req, res) => {
+router.get("/", withAuth, async (req, res) => {
   try {
     const randomPost = await Post.findOne({
-        order: sequelize.literal('rand()'),
-        include: [
-            {
-                model: User,
-                attributes: ['username'],
-            },
-            // {
-            //     model: Comment,
-            //     include: [
-            //         {
-            //             model: User,
-            //             as: 'user',
-            //             attributes: ['username'],
-            //         },
-            //     ],
-            // },
-        ],
+      order: sequelize.literal('rand()'),
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+        // {
+        //     model: Comment,
+        //     include: [
+        //         {
+        //             model: User,
+        //             as: 'user',
+        //             attributes: ['username'],
+        //         },
+        //     ],
+        // },
+      ],
     });
 
-    if(randomPost) {
-        const post = randomPost.get({ plain: true });
-        res.render('homepage', {
-            ...post,
-            loggedIn: req.session.loggedIn
-        })
-        console.log(post);
+
+    // top posts by views. most viewed post
+    const alltopPosts = await Post.findAll({
+      order: [
+        ['views', 'DESC']
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+        // {
+        //     model: Comment,
+        //     include: [
+        //         {
+        //             model: User,
+        //             as: 'user',
+        //             attributes: ['username'],
+        //         },
+        //     ],
+        // },
+      ],
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    if (randomPost && alltopPosts) {
+      const randPost = randomPost.get({ plain: true });
+      const topPosts = alltopPosts.map(post => post.get({ plain: true }));
+
+      var filteredTopPosts = [];  //an empty array
+      for( let a = 0; a < 8; a++) {
+        filteredTopPosts[a] = topPosts[a];  //assigns the values of topPost's  a'th index to the filteredTopPosts array. This array will store only the top 7 posts by view counts.
+      }
+
+
+
+      console.log(filteredTopPosts);
+
+      res.render('homepage', {
+        ...randPost,
+        loggedIn: req.session.loggedIn
+      })
+      // console.log(post);
     } else {
-        res.status(404).json({ message: 'No posts found' });
+      res.status(404).json({ message: 'No posts found' });
     }
 
-} catch (error) {
+  } catch (error) {
     res.status(500).json(error);
-}
-  });
-  
-  // Define the login route
-  router.get("/signup", (req, res) => {
-    res.render("./login/signup");
-  });
+  }
+});
 
 
-router.get('/loginController', async (req, res)=> {
+
+
+
+// Define the login route
+router.get("/signup", (req, res) => {
+  res.render("./login/signup");
+});
+
+
+router.get('/loginController', async (req, res) => {
   try {
     res.render('./login/loginController')
   } catch (error) {
