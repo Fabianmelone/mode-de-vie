d064dfa789fd0033897c018582bf5bbebceace03
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-07-28 22:48:30 -0400

    got handlebars to show the top 7 posts

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -75,13 +75,11 @@ router.get("/", withAuth, async (req, res) => {
       for( let a = 0; a < 8; a++) {
         filteredTopPosts[a] = topPosts[a];  //assigns the values of topPost's  a'th index to the filteredTopPosts array. This array will store only the top 7 posts by view counts.
       }
-
-
-
       console.log(filteredTopPosts);
 
       res.render('homepage', {
         ...randPost,
+        filteredTopPosts,
         loggedIn: req.session.loggedIn
       })
       // console.log(post);
