7c20747dc75ebccc52272ca2c6dc997fcb0d004b
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-07-29 13:56:37 -0400

    buttons on the navbar will redirect to their routes and connect to their api

---
+++ public/js/navbar.js
@@ -0,0 +1,26 @@
+
+document.querySelector('#profileBtn').addEventListener('click', (event)=>{
+    window.location.replace('/user/profile')
+});
+
+document.querySelector('#dashboardBtn').addEventListener('click', (event)=>{
+    window.location.replace('/user/dashboard')
+});
+
+document.querySelector('#rankingBtn').addEventListener('click', (event)=>{
+    window.location.replace('/ranking')
+});
+
+document.querySelector('#messageBtn').addEventListener('click', (event)=>{
+    window.location.replace('/messages')
+});
+
+document.querySelector('#followingbtn').addEventListener('click', (event)=>{
+    window.location.replace('/following')
+});
+
+
+document.querySelector('#savedBtn').addEventListener('click', (event)=>{
+    window.location.replace('/saved')
+});
+
