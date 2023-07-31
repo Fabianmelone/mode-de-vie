bb551ff5adf925123e627d44d7fddd7a3abe49c4
hnm938 <mrbooomboom@gmail.com>
2023-07-31 14:59:54 -0400

    Finished ranking page, now loads data from DB

--- config/helper.js
+++ config/helper.js
@@ -1,9 +1,22 @@
-const Handlebars = require("handlebars");
+const Handlebars = require('handlebars');
 
-Handlebars.registerHelper("helper-log-posts", function (posts, options) {
-  // Log the userPosts data
-  console.log(posts);
+const regexMatch = (pattern, str) => {
+  const regex = new RegExp(pattern, "i"); // 'i' makes it case-insensitive
+  return regex.test(str);
+};
 
-  // Return the block content
-  return options.fn(this);
+// Format the likes and views counts
+Handlebars.registerHelper('formatCount', (count) => {
+  if (count >= 1000 && count < 1000000) {
+    return (count / 1000).toFixed(0) + 'K';
+  } else if (count >= 1000000) {
+    return (count / 1000000).toFixed(0) + 'M';
+  } else {
+    return count;
+  }
 });
+
+module.exports = {
+  regexMatch,
+  Handlebars
+};
