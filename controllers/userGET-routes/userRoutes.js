7aff61fb74f77d090ab053551118148d6a353217
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-07-31 18:31:30 -0400

    made partials for the user page

--- controllers/userGET-routes/userRoutes.js
+++ controllers/userGET-routes/userRoutes.js
@@ -46,25 +46,47 @@ router.get("/", withAuth, async (req, res) => {
       return res.redirect("/");
     }
 
-    // Find all posts belonging to the user
-    const userPosts = await Post.findAll({ where: { user_id: user.id } });
+    let userPosts, userFollowingPosts, userSavedPosts;
+
+    if (user.id) {
+      // Find all posts belonging to the user
+      userPosts = await Post.findAll({ where: { user_id: user.id } });
+    }
+
+    if (user.followingIds) {
+      // Find posts from users the current user is following
+      userFollowingPosts = await Post.findAll({ where: { user_id: user.followingIds } });
+    }
+
+    if (user.savedPostIds) {
+      // Find posts that the user has saved
+      userSavedPosts = await Post.findAll({ where: { id: user.savedPostIds } });
+    }
 
     // Convert sequelize object to plain JavaScript object
     const userData = user.get({ plain: true });
-    const postsData = userPosts.map((post) => post.get({ plain: true }));
+    const postsData = userPosts ? userPosts.map((post) => post.get({ plain: true })) : [];
+    const followingPostsData = userFollowingPosts ? userFollowingPosts.map((post) => post.get({ plain: true })) : [];
+    const savedPostsData = userSavedPosts ? userSavedPosts.map((post) => post.get({ plain: true })) : [];
 
+    console.log(postsData);
     // Render the profile page and pass the user's information and posts to it
     res.render("localuser", {
-        user: userData,
-        posts: postsData,
+      user: userData,
+      userPosts: postsData,
+      userFollowingPosts: followingPostsData,
+      userSavedPosts: savedPostsData,
     });
-
-    console.log(userPosts.map((post) => post.get({ plain: true })));
   } catch (error) {
+    // Log and handle the error
     console.error("Error fetching user data:", error);
     res.status(500).send("Internal Server Error");
   }
 });
+
+
+
+
 
 // Route to display page with users that current user is following and also the top three posts of the user being followed
 router.get("/followingPage", async (req, res) => {
