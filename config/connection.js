de5c5d195e3cd4d31a25f719470c28219d20a00d
PythonFabi <129299589+PythonFabi@users.noreply.github.com>
2023-08-02 12:26:29 -0400

    fix connection setup

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,6 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
